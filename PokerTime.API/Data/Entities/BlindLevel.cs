e494fe69dda16aecc569e379e2f8ae9e988cf344
joespain <F$uguy32304>
2021-07-12 10:37:15 -0400

    Add project files.

---
+++ PokerTime.API/Data/Entities/BlindLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokerTime.API.Data.Entities
+{
+    public class BlindLevel
+    {
+        public int LevelId { get; set; }
+        public int LevelNumber { get; set; }
+        public 
+
+    }
+}
